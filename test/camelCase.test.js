a6ac2c1bbd41549598d20483ebcf409425e504fb
caleoasin312 <zikisa@projectcodex.co>
2018-01-10 15:05:17 +0200

    function that takes in a string and returns in camel case;

---
+++ test/camelCase.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const camelCase = require('../functions/camelCase');
+
+describe('camel case function tests', () => {
+
+    it('should return helloWorld for Hello World', (done) => {
+        assert.equal('helloWorld', camelCase('Hello World'));
+        done();
+    });
+
+    it('should return camelCaseFunction for Camel case Function', (done) => {
+        assert.equal('camelCaseFunction', camelCase('Camel case Function'));
+        done();
+    });
+
+});
