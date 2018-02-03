82e32f184de28793ae4bfc40b0400fe6f37a9721
caleoasin312 <zikisa@projectcodex.co>
2018-02-03 16:01:12 +0200

    removed some console logs from the code;

--- libs/domainName.js
+++ libs/domainName.js
@@ -1,14 +1,12 @@
 module.exports = (url) => {
     let urlHolder = null;
     let checkUrl = url.split('.');
-    console.log(checkUrl)
 
     checkUrl.map(check => {
         if(url.startsWith(checkUrl[0])) {
             urlHolder = checkUrl[1];
         }
     });
-    console.log(urlHolder);
     return urlHolder;
 }
  
